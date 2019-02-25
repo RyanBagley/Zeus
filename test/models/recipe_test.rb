759d5997e7611abfad9790a0b0126d9bc609b090
Ryan Bagley <ryan.bagley@autorama.co.uk>
2019-02-25 11:28:54 +0000

    add one to many association between recipe and chef

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -3,31 +3,37 @@ require 'test_helper'
 class RecipeTest < ActiveSupport::TestCase
 
   def setup
-    @recipe = Recipe.new(name: "meat", description: "great meat recipe")
-  end
+    @chef = Chef.create!(chefname: "ryan", email: "ryan@example.com")
+    @recipe = @chef.recipes.build(name: "meat", description: "great meat recipe")
 
-  test "recipe should be valid" do
-    assert @recipe.valid?
-  end
 
-  test "name should be present" do
-    @recipe.name = " "
-    assert_not @recipe.valid?
-  end
+    test "recipe without chef should be invalid" do
+      @recipe.chef_id = nil
+      assert_not @recipe.valid?
+    end
 
-  test "description should be present" do
-    @recipe.description = " "
-    assert_not @recipe.valid?
-  end
+    test "recipe should be valid" do
+      assert @recipe.valid?
+    end
 
-  test "description shouldn't be less than 5 characters" do
-    @recipe.description = "a" * 3
-    assert_not @recipe.valid?
-  end
+    test "name should be present" do
+      @recipe.name = " "
+      assert_not @recipe.valid?
+    end
 
-  test "description shouldn't be more than 500 characters" do
-    @recipe.description = "a" * 501
-    assert_not @recipe.valid?
-  end
+    test "description should be present" do
+      @recipe.description = " "
+      assert_not @recipe.valid?
+    end
 
+    test "description shouldn't be less than 5 characters" do
+      @recipe.description = "a" * 3
+      assert_not @recipe.valid?
+    end
+
+    test "description shouldn't be more than 500 characters" do
+      @recipe.description = "a" * 501
+      assert_not @recipe.valid?
+    end
+  end
 end
